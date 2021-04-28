08f10b3c982066d0e041930f3c9ad9f05af7f236
Izenda-ericchang <eric.chang@izenda.com>
2021-04-28 13:27:00 -0400

    Skeleton finished

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,7 +3,6 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Controllers
