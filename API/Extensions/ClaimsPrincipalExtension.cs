4b6ba4cb614bba4d32e34efe0d2dd682cf1f5201
Izenda-ericchang <eric.chang@izenda.com>
2021-06-21 15:01:44 -0400

    pagination implementation

--- API/Extensions/ClaimsPrincipalExtension.cs
+++ API/Extensions/ClaimsPrincipalExtension.cs
@@ -7,8 +7,13 @@ namespace API.Extensions
         #region Methods
         public static string GetUsername(this ClaimsPrincipal user)
         {
-            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-        } 
+            return user.FindFirst(ClaimTypes.Name)?.Value;
+        }
+
+        public static int GetUserId(this ClaimsPrincipal user)
+        {
+            return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+        }
         #endregion
     }
 }
---
+++ API/Helper/PaginationHeader.cs
@@ -0,0 +1,25 @@
+﻿namespace API.Helper
+{
+    public class PaginationHeader
+    {
+        #region Properties
+        public int CurrentPage { get; set; }
+
+        public int ItemsPerPage { get; set; }
+
+        public int TotalItems { get; set; }
+
+        public int TotalPages { get; set; }
+        #endregion
+
+        #region CTOR
+        public PaginationHeader(int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            CurrentPage = currentPage;
+            ItemsPerPage = itemsPerPage;
+            TotalItems = totalItems;
+            TotalPages = totalPages;
+        } 
+        #endregion
+    }
+}
